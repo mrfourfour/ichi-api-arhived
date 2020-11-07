b4c22431efb592c4746c6f8712a9b2908306a0ae
aerain <j40f893@gmail.com>
2020-11-07 16:44:07 +0900

    clear: delete unused test

--- ichi-user/src/test/kotlin/com/mrfourfour/ichi/UserApplicationTests.kt
+++
@@ -1,9 +0,0 @@
-package com.mrfourfour.ichi
-
-import org.springframework.boot.autoconfigure.SpringBootApplication
-
-@SpringBootApplication
-class UserApplicationTests {
-    fun contextLoads() {
-    }
-}
