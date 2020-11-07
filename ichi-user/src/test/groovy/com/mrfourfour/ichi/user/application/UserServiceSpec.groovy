cc85dd4fe7240e9abc18181da4272976d762ff8d
aerain <j40f893@gmail.com>
2020-11-07 16:57:31 +0900

    test: add test code

---
+++ ichi-user/src/test/groovy/com/mrfourfour/ichi/user/application/UserServiceSpec.groovy
@@ -0,0 +1,57 @@
+package com.mrfourfour.ichi.user.application
+
+import com.mrfourfour.ichi.keycloak.application.CreateUserFailedException
+import com.mrfourfour.ichi.keycloak.application.TokenProvider
+import spock.lang.Specification
+
+class UserServiceSpec extends Specification {
+
+    UserService sut
+    TokenProvider tokenProvider
+
+    def setup() {
+        tokenProvider = Mock()
+        sut = new UserService(tokenProvider)
+    }
+
+    def "issue token when user login" () {
+        given:
+        LoginParameter loginParameter = new LoginParameter(
+                "hello", "world"
+        )
+
+        when:
+        sut.login(loginParameter)
+
+        then:
+        1 * tokenProvider.issue(*_)
+    }
+
+    def "issue token when user is trying sign up"() {
+        given:
+        SignUpParameter signUpParameter = new SignUpParameter(
+                "hello", "world"
+        )
+
+        when:
+        sut.signUp(signUpParameter)
+
+        then:
+        1 * tokenProvider.signUp(*_)
+        1 * tokenProvider.issue(*_)
+    }
+
+    def "failed to create user when user is trying to sign up"() {
+        given:
+        SignUpParameter signUpParameter = new SignUpParameter(
+                "hello", "world"
+        )
+        tokenProvider.signUp(*_) >> { throw new CreateUserFailedException() }
+
+        when:
+        sut.signUp(signUpParameter)
+
+        then:
+        thrown(CreateUserFailedException.class)
+    }
+}
