2a85004351737715b794ac92786408973a7bfacb
horobito <odkfu1@gmail.com>
2021-10-15 00:25:39 +0900

    fix: change way to get principal

--- ichi-user/src/main/kotlin/com/mrfourfour/ichi/matchmakerproxy/representation/MatchMakerProxyController.kt
+++ ichi-user/src/main/kotlin/com/mrfourfour/ichi/matchmakerproxy/representation/MatchMakerProxyController.kt
@@ -1,21 +1,28 @@
 package com.mrfourfour.ichi.matchmakerproxy.representation
 
+import com.mrfourfour.ichi.keycloak.infrastructure.PrincipalService
 import com.mrfourfour.ichi.matchmakerproxy.application.MatchMakerProxyService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.*
 
 
 @RestController
 class MatchMakerProxyController(
-        private val matchMakerProxyService : MatchMakerProxyService
+        private val matchMakerProxyService : MatchMakerProxyService,
+        private val principalService: PrincipalService
 ) {
 
     @PostMapping("/match/request")
     fun matchRequest() : ResponseEntity<Void> {
-        val userId = SecurityContextHolder.getContext().getAuthentication().principal
-        matchMakerProxyService.request(userId as String)
+
+
+        val principal = principalService.getUserPrincipal()
+        matchMakerProxyService.request(principal.toString())
+
+
         return ResponseEntity.ok().build()
     }
 
