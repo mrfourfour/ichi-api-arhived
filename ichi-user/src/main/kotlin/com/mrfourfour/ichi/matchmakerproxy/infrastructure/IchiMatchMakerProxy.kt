370e19aab32190b8498d3678cb24763fa2223364
horobito <odkfu1@gmail.com>
2021-10-15 00:24:52 +0900

    fix: 1) change response 2) change parameter [UserId -> UserIdDto] 3) apply property

--- ichi-user/src/main/kotlin/com/mrfourfour/ichi/matchmakerproxy/infrastructure/IchiMatchMakerProxy.kt
+++ ichi-user/src/main/kotlin/com/mrfourfour/ichi/matchmakerproxy/infrastructure/IchiMatchMakerProxy.kt
@@ -4,62 +4,71 @@ import com.mrfourfour.ichi.matchmakerproxy.application.MatchMakerProxy
 import org.apache.http.HttpHeaders
 
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ReactiveHttpOutputMessage
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserter
 import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToMono
 import java.lang.IllegalArgumentException
-
-
+import java.util.*
 @Component
 class IchiMatchMakerProxy(
     @Qualifier("matchMakerWebClient")
         private val webClient: WebClient
 ) : MatchMakerProxy {
-    override fun request(userId: String): HttpStatus? {
+    override fun request(userId: String){
+
         val createFormData = createFormData(userId)
-        return fetchRequest(createFormData)
+        fetchRequest(createFormData)
     }
 
-    private fun fetchRequest(userId: BodyInserters.FormInserter<String>): HttpStatus? =
+    private fun fetchRequest(userIdDto: BodyInserter<UserIdDto, ReactiveHttpOutputMessage>) {
         webClient.post()
-                .uri{uriBuilder -> uriBuilder.build()}
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .body(userId)
+                .uri{uriBuilder -> uriBuilder.path("/match/request").build()}
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(userIdDto)
                 .retrieve()
                 .onStatus({status-> status.is4xxClientError}){
                     it.bodyToMono<Throwable>().map { IllegalArgumentException() }
                 }
-                .bodyToMono<HttpStatus>(HttpStatus::class.java)
+                .bodyToMono<Unit>()
                 .block();
-
-
-    private fun createFormData(userId: String): BodyInserters.FormInserter<String> {
-        return BodyInserters.fromFormData("user_id", userId)
-
     }
 
 
 
+    private fun createFormData(userId: String): BodyInserter<UserIdDto, ReactiveHttpOutputMessage> {
+        val userIdDto = UserIdDto(userId)
+        return BodyInserters.fromValue(userIdDto)
+    }
+
 }
+
+
+
+data class UserIdDto(
+        val userId: String
+)
 
 @Configuration
 class MatchMakerWebClientConfig{
 
     @Bean
     fun matchMakerWebClient(
+           @Value("\${matchmaker.url}")
+           matchMakerUrl: String
     ): WebClient {
-        val baseUrl = getBaseUrl()
+
         return WebClient
                 .builder()
-                .baseUrl(baseUrl)
+                .baseUrl(matchMakerUrl)
                 .build()
     }
-
-    private fun getBaseUrl() = "172.30.1.12:port/match/request"
 
 }
